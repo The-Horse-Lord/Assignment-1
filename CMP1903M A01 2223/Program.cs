f62339f529287be243c33b07aef408b87485ae35
The-Horse-Lord <127056433+The-Horse-Lord@users.noreply.github.com>
2023-03-09 12:16:18 +0000

    Update Program.cs

--- CMP1903M A01 2223/Program.cs
+++ CMP1903M A01 2223/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,15 +11,46 @@ namespace CMP1903M_A01_2223
         static void Main(string[] args)
         {
             Testing test = new Testing();
-            Console.WriteLine("Fisher-Yates Shuffle: \n\n");
-            Testing.TestingShuffle(1);
-            Console.WriteLine("Riffle Shuffle: \n\n");
-            Testing.TestingShuffle(2);
-            Console.WriteLine("No Shuffle: \n\n");
-            Testing.TestingShuffle(3);
-            Testing.TestingDeal(1);
-            Testing.TestingDeal(5);
-            Console.ReadLine();
+            Pack pack = new Pack();
+            //The user can choose the type of shuffle and how many cards to deal
+            shuffle: Console.WriteLine("Choose the shuffle method to show the pack: \n1)Fisher-Yates Shuffle \n2)Riffle Shuffle\n3)No Shuffle\n");
+            try
+            {
+                int choice = Convert.ToInt32(Console.ReadLine());
+                if (choice > 0 && choice < 4) pack = Testing.TestingShuffle(choice);
+                else
+                {
+                    Console.WriteLine("Invalid command.Try again?\n\n");
+                    goto shuffle;
+                }
+            }
+            //if the user entered incorrect type of choice, they will be trhown back to the beginning of the code
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Try again?\n\n"); goto shuffle;
+            }
+            cards: Console.WriteLine("Deal a specified amount of cards?\n");
+            try
+            {
+                int amount = Convert.ToInt32(Console.ReadLine());
+                if (amount > 0 && amount < 52)
+                {
+                    Testing.TestingDeal(amount, pack);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid command.Try again?\n\n");
+                    goto cards;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Try again?\n\n"); goto cards;
+            }
+            }
         }
     }
-}
+
